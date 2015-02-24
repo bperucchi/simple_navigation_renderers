2e04cad6259e609654df73736017e8f7ec9037ad
Breno Perucchi <bperucchi@gmail.com>
2015-02-23 22:49:00 -0300

    new breadcrumb

--- lib/simple_navigation_renderers.rb
+++ lib/simple_navigation_renderers.rb
@@ -1,6 +1,7 @@
 require "simple-navigation"
 require "simple_navigation_renderers/exceptions"
 require "simple_navigation_renderers/bootstrap"
+require "simple_navigation_renderers/bootstrap_breadcrumb"
 require "simple_navigation_renderers/engine" if defined? Rails::Engine
 require "simple_navigation_renderers/version"
 
@@ -9,3 +10,4 @@ end
 
 SimpleNavigation.register_renderer( bootstrap2: SimpleNavigationRenderers::Bootstrap2 )
 SimpleNavigation.register_renderer( bootstrap3: SimpleNavigationRenderers::Bootstrap3 )
+SimpleNavigation.register_renderer( breadcrumb3: SimpleNavigationRenderers::BootstrapBreadcrumbs )
